c4ed0773bfb7c183298402923e1db98d1aa9cb7f
Priyanka Shah <priyanka2003shah@gmail.com>
2022-06-27 15:28:49 -0400

    set up and stuff from canvas

---
+++ src/UserInterface.js
@@ -0,0 +1,16 @@
+import React from "react"
+import CollectorButton from './CollectorButton'
+
+export default function UserInterface(){
+    return(
+        <div>
+<h2> blah blah blah</h2>
+        <h2> words</h2>
+        <CollectorButton />
+        <h2> more words</h2>
+        
+        </div>
+        
+
+    )
+}
